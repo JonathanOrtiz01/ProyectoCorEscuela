0e1f6dcc5f9786313a4ac0fea3d634c7acd53f9a
Jonathan Ortiz <dual.jonathan.ortiz@gmail.com>
2022-05-13 12:34:17 -0500

    Lista de objetos polimorfica

--- Program.cs
+++ Program.cs
@@ -16,29 +16,7 @@ namespace CorEscuela
             Printer.EscribirTitulo("BIENVENIDOS AL ITSSY");
             Printer.Beep();
             ImprimirCursosEscuela(engine.Escuela);
-
-            Printer.DibujarLinea();
-            Printer.EscribirTitulo("Pruebas de polimorfismo");
-            var alumnoTest = new Alumno{Nombre = "Jonathan Ortiz"};
-
-            ObjetoEscuelaBase ob = alumnoTest;
-            
-            Printer.EscribirTitulo("Alumno");
-            WriteLine($"Alumno: {alumnoTest.Nombre}");
-            WriteLine($"Alumno: {alumnoTest.UniqueId}");
-            WriteLine($"Alumno: {alumnoTest.GetType()}");
-
-            Printer.EscribirTitulo("ObjetoEscuela");
-            WriteLine($"Alumno: {ob.Nombre}");
-            WriteLine($"Alumno: {ob.UniqueId}");
-            WriteLine($"Alumno: {ob.GetType()}");
-
-            var objDummy = new ObjetoEscuelaBase(){Nombre = "Frank Underwood"};
-            Printer.EscribirTitulo("ObjetoEscuelaBase");
-            WriteLine($"Alumno: {objDummy.Nombre}");
-            WriteLine($"Alumno: {objDummy.UniqueId}");
-            WriteLine($"Alumno: {objDummy.GetType()}");
-            
+            var listaObjetos = engine.GetObjetosEscuela();
         }
 
         private static void ImprimirCursosEscuela(Escuela escuela)
